d79821f59f8fd719ffd7520a52657750e967b1b6
Giftedkay <giftkay0@gmail.com>
2022-09-13 14:52:54 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet: prints the lowercase alphabets
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char j;
+
+	for (j = 'a' ; j <= 'z' ; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
