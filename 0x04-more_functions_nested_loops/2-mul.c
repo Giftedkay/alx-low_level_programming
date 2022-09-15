8a87610714d8abad21b2eff3569c293e0faf0c13
Giftedkay <giftkay0@gmail.com>
2022-09-15 02:31:57 -0700

    hot fix

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two integers
+ * @a: argument 1
+ * @b: argument 2
+ *
+ * Return: Success and integer
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
