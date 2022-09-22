9d433deb39568c168d4d738db01eeae5af0507a1
Giftedkay <giftkay0@gmail.com>
2022-09-22 05:14:14 -0700

    trial

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,27 +2,28 @@
 
 /**
  * *rot13 - function that encodes a string using rot13
+ * @s: input string
  *
  * Return: a ponter to the encoded string
  */
 
-char *rot13(char *str)
+char *rot13(char *s)
 {
-	int index1 = 0, index2;
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-	char rot13[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+	int count = 0, i;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVCWXYZABCDEFGHIJKLM";
 
-	while (str[index1])
+	while (*(s + count) != '\0')
 	{
-		for (index2 = 0; index2 < 52; index2++)
+		for (i = 0; i < 52; i++)
 		{
-			if (str[index1] == alphabet[index2])
+			if (*(s + count == alphabet[i]))
 			{
-				str[index1] = rot13key[index2];
+				*(s + count) = rot13[i];
 				break;
 			}
 		}
-		index1++;
+		count++;
 	}
-	return (str);
+	return (s);
 }
