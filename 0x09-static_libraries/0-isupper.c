f1720e6eef6ae8e5609655c6c541f0af850e72fb
Giftedkay <giftkay0@gmail.com>
2022-09-30 14:47:05 -0700

    task 0

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - function to check uppercase alphas
+ * @c: an integer parameter
+ *
+ * Return: Always 0
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: argument
+ *
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = -n;
+	return (n);
+}
