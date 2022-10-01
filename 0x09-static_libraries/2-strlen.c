97265f3a5b7bb423308fe45135ebd8731a5c0969
Giftedkay <giftkay0@gmail.com>
2022-09-30 23:34:01 -0700

    hot fix

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: string pointer to be passed to the function
+ *
+ * Return: returns length of the string
+ *
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - checks is alphabet is lower alphabet
+ * @c: working operand
+ *
+ * Return: Returns 1 or 0 depending on the condition
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
