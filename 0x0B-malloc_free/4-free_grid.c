5751c53ec1c448173dfd0b6e0374562e77de5965
Giftedkay <giftkay0@gmail.com>
2022-10-04 14:32:25 -0700

    hot fix

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,7 +1,9 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * free_grid -  frees a 2 dimensional grid previously created by your alloc_grid function
+ * free_grid -  frees a 2 dimensional grid previously created
+ * by your alloc_grid function
  * @grid: pointer to pointer
  * @height: height of grid
  *
