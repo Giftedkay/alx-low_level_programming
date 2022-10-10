a8b4a730c64c3ad1030d39bac71c9299749952df
Giftedkay <giftkay0@gmail.com>
2022-10-10 14:04:25 -0700

    hot fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,7 +6,7 @@ char *_strcopy(char *dest, char *src);
 dog_t *new_dog(char *name, float age, char *owner);
 
 /**
- * _strlen - finds the length of a string
+ * _strlen - find the length of a string
  * @str: the string to be measured
  *
  * Return: the length of the string
@@ -16,14 +16,14 @@ int _strlen(char *str)
 {
 	int len = 0;
 
-	while (str++)
+	while (*str++)
 		len++;
 	return (len);
 }
 
 /**
- * _strcopy - copies a string pointed to by src, including
- * the terminating null byte,to a buffer pointed to by dest.
+ * *_strcopy - copies a string pointed to by src, including
+ * the terminating null byte,to a buffer pointed to by dest
  * @dest: the buffer storing the string copy
  * @src: the source string
  *
@@ -41,7 +41,7 @@ char *_strcopy(char *dest, char *src)
 }
 
 /**
- * ne_dog - create a new dog
+ * new_dog - creates a new dog
  * @name: the name of the dog
  * @age: the age of the dog
  * @owner: the owner of the dog
@@ -52,7 +52,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *doggo;
 
-	if (name == NULL || owner == NULL)
+	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
 	doggo = malloc(sizeof(dog_t));
 	if (doggo == NULL)
@@ -75,3 +75,4 @@ dog_t *new_dog(char *name, float age, char *owner)
 	doggo->owner = _strcopy(doggo->owner, owner);
 	return (doggo);
 }
+
