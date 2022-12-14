5780b3ffca832c4bb9011f368127a1fa66029de7
Giftedkay <giftkay0@gmail.com>
2022-12-14 06:14:22 -0800

    task o

---
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strchr - function that locates a character in a string
+ * @s: string given
+ * @c: another char
+ * Return: a string
+ */
+
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s[index] >= '\0'; index++)
+	{
+		if (s[index] == c)
+			return (s + index);
+		}
+	return ('\0');
+}
